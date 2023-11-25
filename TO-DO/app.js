efce863ac1bc0403428adf5f1d0590704dc93fca
Prasanna <112683238+Prasanna021102@users.noreply.github.com>
2023-11-25 17:35:11 +0530

    Add files via upload

---
+++ TO-DO/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+// Store tasks in an in-memory array (for demonstration purposes)
+let tasks = [];
+
+// Endpoint to get all tasks
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
+});
+
+// Endpoint to add a task
+app.post('/tasks', (req, res) => {
+  const task = req.body.task;
+  if (task.trim() !== '') {
+    tasks.push(task);
+    res.status(201).send('Task added successfully');
+  } else {
+    res.status(400).send('Task cannot be empty');
+  }
+});
+
+const PORT = 3000; // You can use any port you prefer
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
